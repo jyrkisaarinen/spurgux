1354b5357fbefd6e8b23f037facc01751e53da7f
jsaarinen <jsaarinen@a1406c9e-6cc2-471f-9a9f-5efa6819043b>
2015-04-18 13:32:52 +0000

    Added accessors.

--- trunk/src/main/java/fi/jsaarinen/spurgux/Player.java
+++ trunk/src/main/java/fi/jsaarinen/spurgux/Player.java
@@ -6,5 +6,46 @@ public class Player
   private int points;
   private int xpos, ypos;
     
+  public Player(double ethanolLevel, int points, int xpos, int ypos)
+  {
+    super();
+    this.ethanolLevel = ethanolLevel;
+    this.points = points;
+    this.xpos = xpos;
+    this.ypos = ypos;
+  }
+  public double getEthanolLevel()
+  {
+    return this.ethanolLevel;
+  }
+  public void setEthanolLevel(double ethanolLevel)
+  {
+    this.ethanolLevel = ethanolLevel;
+  }
+  public int getPoints()
+  {
+    return this.points;
+  }
+  public void setPoints(int points)
+  {
+    this.points = points;
+  }
+  public int getXpos()
+  {
+    return this.xpos;
+  }
+  public void setXpos(int xpos)
+  {
+    this.xpos = xpos;
+  }
+  public int getYpos()
+  {
+    return this.ypos;
+  }
+  public void setYpos(int ypos)
+  {
+    this.ypos = ypos;
+  }
+    
     
 }
