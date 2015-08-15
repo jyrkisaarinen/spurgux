ce06a5a5ac1eb6332247928036ef70ae708677e6
jsaarinen <jsaarinen@a1406c9e-6cc2-471f-9a9f-5efa6819043b>
2015-08-15 14:16:38 +0000

    Fixed indentation.

--- src/main/java/fi/jsaarinen/spurgux/rakennukset/Apteekki.java
+++ src/main/java/fi/jsaarinen/spurgux/rakennukset/Apteekki.java
@@ -1,6 +1,34 @@
 package fi.jsaarinen.spurgux.rakennukset;
 
+import fi.jsaarinen.spurgux.Canvas;
+import fi.jsaarinen.spurgux.Player;
+
 public class Apteekki
 {
+  private String[] map =
+    {
+      "############",
+      "# APTEEKKI #",
+      "#          #",
+      "#          #",
+      "############"    
+    };
+    
+  public void visit(Player player, Canvas canvas)
+  {
+    if (player.getEthanolLevel() >= 1.5)
+    {
+      canvas.renderFeedbackLine("Pameja ei voi vetää viinan kanssa sekaisin!");
+    }
+    else
+    {        
+      canvas.renderFeedbackLine("Diatsepaami parantaa oloasi.");
+      player.setWellBeing(player.getWellBeing() + 2);
+    }
+  }
 
+  public String[] getMap()
+  {
+    return this.map;
+  }
 }
