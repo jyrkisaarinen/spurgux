4bc78f36f1fc63fa0e2c3faf4a39d6b03840d2d2
jsaarinen <jsaarinen@a1406c9e-6cc2-471f-9a9f-5efa6819043b>
2015-09-02 14:23:50 +0000

    Compiles again.

--- src/main/java/fi/jsaarinen/spurgux/rakennukset/MummonBaari.java
+++ src/main/java/fi/jsaarinen/spurgux/rakennukset/MummonBaari.java
@@ -1,20 +1,23 @@
 package fi.jsaarinen.spurgux.rakennukset;
 
+import fi.jsaarinen.spurgux.Canvas;
+import fi.jsaarinen.spurgux.Player;
+
 public class MummonBaari extends AbstractRakennus
 {
-  public MummonBaari()
+  public MummonBaari(int x, int y, int width, int height)
   {
-    super();
-  }
-
-  public MummonBaari(int x, int y, int width, int height, String name)
-  {
-    super(x, y, width, height, name);
+    super(x, y, width, height, "Mummon baari");
   }
 
   @Override
   public String[] getMap()
   {
     return null;
+  }
+
+  @Override
+  public void visit(Player player, Canvas canvas)
+  {    
   }
 }
