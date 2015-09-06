a1419195386187f5332c2a55834d55f414fba66c
jsaarinen <jsaarinen@a1406c9e-6cc2-471f-9a9f-5efa6819043b>
2015-09-06 19:10:17 +0000

    Added.

---
+++ psypotti/src/main/java/fi/jsaarinen/psypotti/db/PatientNotFoundException.java
@@ -0,0 +1,34 @@
+package fi.jsaarinen.psypotti.db;
+
+public class PatientNotFoundException extends Exception
+{
+  public PatientNotFoundException()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+  public PatientNotFoundException(String message)
+  {
+    super(message);
+    // TODO Auto-generated constructor stub
+  }
+
+  public PatientNotFoundException(Throwable cause)
+  {
+    super(cause);
+    // TODO Auto-generated constructor stub
+  }
+
+  public PatientNotFoundException(String message, Throwable cause)
+  {
+    super(message, cause);
+    // TODO Auto-generated constructor stub
+  }
+
+  public PatientNotFoundException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace)
+  {
+    super(message, cause, enableSuppression, writableStackTrace);
+    // TODO Auto-generated constructor stub
+  }
+}
