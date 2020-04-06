5827ca47cae82ecb8cc24bbd640d2b1305c69266
Jyrki Saarinen <jyrki.olavi.saarinen@gmail.com>
2020-04-06 18:38:18 +0300

    Spring Boot and upgraded MySQL driver

--- src/main/java/fi/jsaarinen/spurgux/Application.java
+++ src/main/java/fi/jsaarinen/spurgux/Application.java
@@ -1,5 +1,42 @@
 package fi.jsaarinen.spurgux;
 
-public class Application {
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx)
+  {
+    {
+      System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+      String[] beanNames = ctx.getBeanDefinitionNames();
+      Arrays.sort(beanNames);
+      for (String beanName : beanNames)
+      {
+        System.out.println(beanName);
+      }
+
+    };
+    return null;
+  }
+
+}
+
+public class Application
+{
 
 }
